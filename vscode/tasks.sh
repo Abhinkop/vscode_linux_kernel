ac5042d5db3ee5889d556b163169b019d3004495
Abhin Parekadan Jose <abhinjoses@gmali.com>
2026-04-26 11:17:00 +0200

    task.sh: Add building of kernel for ubuntu

--- vscode/tasks.sh
+++ vscode/tasks.sh
@@ -23,6 +23,39 @@ build_kernel_clang_arm64(){
 	make O=${BUILD_DIR} ARCH=arm64 LLVM=1 LLVM_IAS=1 -j$(nproc) Image
 }
 
+configure_ubuntu_local(){
+	mkdir -p ${BUILD_DIR}
+	# from https://oneuptime.com/blog/post/2026-03-02-how-to-build-and-install-a-custom-kernel-on-ubuntu/view
+	cp /boot/config-$(uname -r) ${BUILD_DIR}/.config
+	set +e
+	make O=${BUILD_DIR} olddefconfig
+	set -e
+
+	# Enable Android Binder
+	scripts/config --enable CONFIG_ANDROID_BINDER_IPC --file ${BUILD_DIR}/.config
+	scripts/config --enable CONFIG_ANDROID_BINDERFS --file ${BUILD_DIR}/.config
+	scripts/config --set-str CONFIG_ANDROID_BINDER_DEVICES "" --file ${BUILD_DIR}/.config
+
+	# Disable debug info to speed up build and reduce size
+	scripts/config --disable CONFIG_DEBUG_INFO --file ${BUILD_DIR}/.config
+	scripts/config --disable CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT --file ${BUILD_DIR}/.config
+	scripts/config --enable CONFIG_DEBUG_INFO_NONE --file ${BUILD_DIR}/.config
+
+	# Ubuntu's kernel config requires a trusted key for module signing.
+	scripts/config --set-str CONFIG_SYSTEM_TRUSTED_KEYS "" --file ${BUILD_DIR}/.config
+	scripts/config --set-str CONFIG_SYSTEM_REVOCATION_KEYS "" --file ${BUILD_DIR}/.config
+}
+
+build_ubuntu_local(){
+	# (Binary Only)
+	make O=${BUILD_DIR} LOCALVERSION=-custom -j$(nproc) bindeb-pkg
+}
+
+build_ubuntu_local_with_src_tar(){
+	# (Source + Binary)
+	make O=${BUILD_DIR} LOCALVERSION=-custom-with-src -j$(nproc) deb-pkg
+}
+
 configure_kernel_gcc_arm64(){
 	make O=${BUILD_DIR} ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- ${CONFIG_FILE}
 }
@@ -33,6 +66,17 @@ build_kernel_gcc_arm64(){
 
 build_compile_commands() {
 	python3 ./scripts/clang-tools/gen_compile_commands.py -d ${BUILD_DIR}
+}
+
+launch_arm_virt() {
+	qemu-system-aarch64 \
+		-M virt \
+		-cpu cortex-a57 \
+		-smp 4 \
+		-m 2G \
+		-kernel ${BUILD_DIR}/arch/arm64/boot/Image \
+		-append "console=ttyAMA0 earlycon" \
+		-nographic
 }
 
 # Function: Build the kernel
@@ -94,6 +138,26 @@ case "${1:-}" in
 	configure-clang)
 		shift
 		configure_kernel_arm_virt "clang"
+		;;
+	configure-ubuntu-local)
+		shift
+		configure_ubuntu_local
+		;;
+	build-ubuntu-local)
+		shift
+		build_ubuntu_local
+		;;
+	build-ubuntu-local-with-src-tar)
+		shift
+		build_ubuntu_local_with_src_tar
+		;;
+	configure-ubuntu-local)
+		shift
+		configure_ubuntu_local
+		;;
+	launch-arm-virt)
+		shift
+		launch_arm_virt
 		;;
 	clean)
 		log_info "Cleaning up..."
